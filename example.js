69c47f4e5ecfa9b4bcfb69d32a47ef04c21700e5
RzR <rzr@users.sf.net>
2018-03-10 12:54:59 +0100

    WIP: mod: example.js (sandbox/rzr/devel/master)
    Signed-off-by: RzR <rzr@users.sf.net>

--- example.js
+++ example.js
@@ -1,9 +1,21 @@
 var MPU6050 = require("index.js");
 
-var sensor = MPU6050();
+var sensor = new MPU6050();
 console.log("sensor: ");
 
-sensor.initialize( function() {
-    console.log("sensor: ");
+sensor.initialize();
+sensor.getAcceleration(function(err, res) {
+    if (!err) console.log(res);
   });
+
+
+// sensor.initialize( function(err) {
+//     console.log("sensor: ");
+//     if (!err) {
+//       sensor.getAcceleration(function(err, res) {
+//           if (!err) console.log(res);
+//         });
+//     }
+//   });
+
 
